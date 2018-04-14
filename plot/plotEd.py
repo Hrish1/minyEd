4fad5e8d45888c9488cabec41e0ee18c40cab6a7
Hrish1 <hmail17.95@gmail.com>
2018-04-14 20:18:41 +0530

    made PEP8 happy

--- plot/plotEd.py
+++ plot/plotEd.py
@@ -1,76 +1,68 @@
 import xml.etree.cElementTree as ET
 
+
 class Links:
 
-	def __init__(self, source, target, edgeId):
-		
-		#continue from here for making Link objects, not used so far
-		self.source = source
-		self.target = target
-		
-		self.edgeId = edgeId
-		
+    def __init__(self, source, target, edgeId):
+        # continue from here for making Link objects, not used so far
+        self.source = source
+        self.target = target
 
-		
-
-class TopoVisualiser:
-
-	def __init__( self, nodes, links={} ):
-		
-		#Links need to be added
-		self.nodes = nodes
-		self.links = links
+        self.edgeId = edgeId
 
 
-	def setNode( self, node ):
-		
-		Node = ET.Element( "node", id=self.nodes.get(node) )		
-		return Node
+class TopologyVisualiser:
+
+    def __init__(self, nodes, links={}):
+        # Links need to be added
+        self.nodes = nodes
+        self.links = links
+
+    def setNode(self, node):
+        Node = ET.Element("node", id=self.nodes.get(node))
+        return Node
+
+    # def setLink( self, source, target, edgeId ):
+
+    # link = Links( source, target, edgeId )
+    # self.edges[link.edgeId]=link
+
+    def buildElements(self):
+        graphml = ET.Element("graphml", xmlns="http://graphml.graphdrawing.org/xmlns")
+        graphml.set("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+        graphml.set("xsi:schemaLocation",
+                    "http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd")
+
+        nodeKey = ET.SubElement(graphml, "key", id="d0")
+        nodeKey.set("for", "node")
+        nodeKey.set("yfiles.type", "nodegraphics")
+
+        edgeKey = ET.SubElement(graphml, "key", id="d1")
+        edgeKey.set("for", "edge")
+        edgeKey.set("yfiles.type", "edgegraphics")
+
+        graph = ET.SubElement(graphml, "graph", id="G")
+        graph.set("edgedefault", "undirected")
+        # node = ET.SubElement(graph ,"node", id="n0")
+        # node = ET.SubElement(graph ,"node", id="n1")
+        # node = ET.SubElement(graph ,"node", id="n2")
+
+        for node in self.nodes:
+            Node = self.setNode(node)
+            graph.append(Node)
+
+        link = ET.SubElement(graph, "edge", id="e0")
+        link.set("source", "n0")
+        link.set("target", "n1")
+        link = ET.SubElement(graph, "edge", id="e1")
+        link.set("source", "n0")
+        link.set("target", "n2")
+
+        Tree = ET.ElementTree(graphml)
+        Tree.write("test.graphml", encoding="UTF-8")
 
 
-	#def setLink( self, source, target, edgeId ):
-
-		#link = Links( source, target, edgeId )
-		#self.edges[link.edgeId]=link
-
-	def buildElements(self):
-	
-		graphml = ET.Element("graphml",xmlns="http://graphml.graphdrawing.org/xmlns")
-		graphml.set("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance")
-		graphml.set("xsi:schemaLocation","http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd")
-
-		nodeKey = ET.SubElement(graphml ,"key", id="d0")
-		nodeKey.set("for", "node")
-		nodeKey.set("yfiles.type", "nodegraphics")
-
-		edgeKey = ET.SubElement(graphml ,"key", id="d1")
-		edgeKey.set("for", "edge")
-		edgeKey.set("yfiles.type", "edgegraphics")
-
-		graph = ET.SubElement(graphml ,"graph", id="G")
-		graph.set("edgedefault","undirected")
-		#node = ET.SubElement(graph ,"node", id="n0")
-		#node = ET.SubElement(graph ,"node", id="n1")
-		#node = ET.SubElement(graph ,"node", id="n2")
-
-		for node in self.nodes:
-			Node = self.setNode( node )
-			graph.append( Node )
-
-		link = ET.SubElement(graph, "edge", id="e0")
-		link.set("source","n0")
-		link.set("target","n1")
-		link = ET.SubElement(graph, "edge", id="e1")
-		link.set("source","n0")
-		link.set("target","n2")
-	
-		Tree = ET.ElementTree(graphml)
-        	Tree.write("test.graphml", encoding ="UTF-8")
-
-
-
-#Remove this 
+# Remove this
 if __name__ == '__main__':
-	obj = TopoVisualiser( nodes={"node1":"n0", "node2":"n1", "node3":"n2" } )
-	obj.buildElements()
-
+    obj = TopologyVisualiser(nodes={"node1": "n0", "node2": "n1", "node3": "n2"})
+    obj.buildElements()
